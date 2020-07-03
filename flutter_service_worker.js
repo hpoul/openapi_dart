7da714c92087790efb5caee6d867701273c63d74
hpoul <hpoul@users.noreply.github.com>
2020-07-03 07:56:58 +0000

    deploy: be54c86b53358d38709062a2ffb7d6f909728c62

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "3443e3fe8ef61f51c3653154257d44e6",
+  "main.dart.js": "13b33ce4d712760d7e5a1e900cb3eaf1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "7c26ffd0c8cb6fd798dffd687f442760",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
