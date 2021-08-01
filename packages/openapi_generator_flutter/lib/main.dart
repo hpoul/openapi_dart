42afedc8ec71db92720cf68b501ee34712f6feb1
Herbert Poul <herbert@codeux.design>
2021-08-01 23:20:26 +0200

    update dependencies in example flutter application.

--- packages/openapi_generator_flutter/lib/main.dart
+++ packages/openapi_generator_flutter/lib/main.dart
@@ -58,8 +58,8 @@ class _OpenApiGeneratorState extends State<OpenApiGenerator> {
       body: Column(
         children: [
           Markdown(
-            onTapLink: (link) {
-              launch(link);
+            onTapLink: (text, href, title) {
+              launch(href);
             },
             shrinkWrap: true,
             data: '''
