9f84f2cdf610d7469494940fc381c0a85c07769d
Bonnyfication <philipp.bonny@googlemail.com>
2022-11-26 23:29:57 +0100

    Add missing parameter to startServer-Method

--- packages/openapi_base/lib/src/server/openapi_server_base.dart
+++ packages/openapi_base/lib/src/server/openapi_server_base.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 abstract class OpenApiServerBase {
   Future<StoppableProcessBase> startServer({
     String address = 'localhost',
     int port = 8080,
+    SecurityContext? securityContext,
+    int? backlog,
+    bool shared = false,
+    String? poweredByHeader = 'Dart with package:shelf',
   });
 }
 
