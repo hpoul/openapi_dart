0634ace2b780e2706e233e6ec72be8aac918da2a
vladimir.koltunov <progrand@gmail.com>
2023-03-03 20:58:31 +0300

    + formatting

--- packages/openapi_code_builder/lib/openapi_code_builder.dart
+++ packages/openapi_code_builder/lib/openapi_code_builder.dart
@@ -4,13 +4,10 @@ import 'package:openapi_code_builder/src/openapi_code_builder.dart';
 export 'package:openapi_code_builder/src/openapi_code_builder.dart'
     show OpenApiLibraryGenerator, OpenApiCodeBuilderUtils;
 
-Builder openapiCodeBuilder(BuilderOptions options) {
-  return OpenApiCodeBuilder(
-    orderDirectives: true,
-    useNullSafetySyntax: true,
-    generateProvider: options.config['generateProvider'] as bool? ?? false,
-    providerNamePrefix: options.config['providerNamePrefix'] as String? ?? '',
-    ignoreSecuritySchemes:
-        options.config['ignoreSecuritySchemes'] as bool? ?? false,
-  );
-}
+Builder openapiCodeBuilder(BuilderOptions options) => OpenApiCodeBuilder(
+      orderDirectives: true,
+      useNullSafetySyntax: true,
+      generateProvider: options.config['generateProvider'] as bool? ?? false,
+      providerNamePrefix: options.config['providerNamePrefix'] as String? ?? '',
+      ignoreSecuritySchemes: options.config['ignoreSecuritySchemes'] as bool? ?? false,
+    );
